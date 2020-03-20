8b69254ebca84172cbe6e2ab4a888dd07348a552
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-03-19 19:40:00 -0500

    Added if statements, added ReadMe, added generate password, added Do/while loops

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # homework-3
-Password Generator
+# 03 JavaScript: Password Generator
+Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+# User Story
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+Acceptance Criteria
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+# What I Did
+* First thing I did was to start out making an array for all the values needed for the program.
+* I then started putting if/else statements for the answer to the prompts introduced to the users.
+* I made the prompts confirms for the choices, with the length being an user input box to determine the length.
+* I then made the for loop for the generate password function. It should incorparte the length necessary for the password.
+* I was running into issues of getting all the arrays to combine so I had to make a new global array to concat to.
+* To solve the issue of the length being accepted regardless of the paramaters, I had to use a do/while loop to force the user answer correctly.
+* I also had to add another so the non numbers were not clearing the prompts.
+* The whole program should be responsive to the user letting them chose from all of the different choices and generate a password 8-128 characaters.
--- script.js
+++ script.js
@@ -1,64 +1,85 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numbers = [1,2,3,4,5,6,7,8,9,0];
-var character = ["!","@","#","$","%","^","&","*"]
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var character = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
+var chosenThings = [];
+var length;
 
 
 
-var useLetters= confirm("Do you want to use letters in your password?")
-var useCapitals= confirm("Do you want to use capital letters?")
-var useNumbers = confirm("Do you want to use numbers in your password?")
-var useCharacters= confirm("Do you want to use charaters in your password?")
-prompt("How long do you want your password to be? *Must be between 8-128*")
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  do {
+    var useLetters = confirm("Do you want to use letters in your password?")
+    var useCapitals = confirm("Do you want to use capital letters?")
+    var useNumbers = confirm("Do you want to use numbers in your password?")
+    var useCharacters = confirm("Do you want to use charaters in your password?")
+
+    if (useLetters == false && useCapitals == false && useNumbers == false && useCharacters == false) {
+      alert("You can't do that! Try again!")
+
+    }
+  }
+  while (useLetters == false && useCapitals == false && useNumbers == false && useCharacters == false)
+
+  do {
+    length = prompt("How long do you want your password to be? *Must be between 8-128*")
+  }
+  while (length < 8 || length > 128 || isNaN(length));
+
+
+
+  if (useLetters == true) {
+
+    chosenThings = chosenThings.concat(letters);
+  } else (useLetters == false);
+
+  if (useCapitals == true) {
+
+    chosenThings = chosenThings.concat(upperCase);
+  } else (useCapitals == false);
+
+  if (useNumbers == true) {
+
+    chosenThings = chosenThings.concat(numbers);
+  } else (useNumbers == false);
+
+  if (useCharacters == true) {
+
+    chosenThings = chosenThings.concat(character);
+  } else (useCharacters == false);
+
+
+
+  var password = generatePassword()
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-  if (useLetters == true && useCapitals==true && useNumbers==true&& useCharacters==true){
-    // code using all.
-  }
-  else if (useLetters == true && useCapitals== true && useNumbers ==true){
-    // code using only letters numbers and capital letters
-  }
-  else if (useLetters == true && useCapitals== true){
-    // code using only lower case and capital letters
-  }
-  else if (useLetters== true){
-    // only using lower case letters
-  }
-  else if(useCapitals==true){
-    // code using only capital letters
-  }
-  else if(useNumbers==true){
-    // code using only numbers
-  }
-  else if(useCharacters== true){
-    // code using only characters
-  }
-
-
 }
 
+function generatePassword() {
+  // alert ("generate password")
+  var password = "";
+  for (var index = 0; index < parseInt(length); index++) {
+
+    var character = Math.floor(Math.random() * chosenThings.length);
+    password += chosenThings[character];
+
+  }
+
+  return password;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+
+
+
+
+
